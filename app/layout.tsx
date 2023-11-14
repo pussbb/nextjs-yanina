9a9a524f9c6f6248c95809f22c1a2be61dbeb50a
pussbb <pussbb@gmail.com>
2023-11-14 18:28:48 +0200

    page encoding fix

--- app/layout.tsx
+++ app/layout.tsx
@@ -27,7 +27,7 @@ export default function RootLayout({
   children: React.ReactNode
 }) {
   return (
-    <html lang="en">
+    <html lang="uk">
       <body className={poppins.className}>
       <Header />
         {children}
