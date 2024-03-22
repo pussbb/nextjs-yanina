a8cd0aac9abb667555184cd627e10ae7446bc75a
pussbb <pussbb@gmail.com>
2024-03-22 09:24:57 +0200

    title for the calendar

---
+++ app/calendar/layout.tsx
@@ -0,0 +1,7 @@
+import {Metadata} from "next";
+import generateDefaultMetadata from "@/app/metadata";
+import Page from "./page";
+
+export const metadata: Metadata = generateDefaultMetadata('Calendar for course «Здорова стопа»')
+
+export default Page
