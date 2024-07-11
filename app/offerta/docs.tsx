4531f34c776fde326fcdb5e5fd7962ba2e503050
pussbb <pussbb@gmail.com>
2024-07-11 09:34:21 +0200

    tmp

--- app/offerta/docs.tsx
+++ app/offerta/docs.tsx
@@ -1,26 +1,20 @@
-"use client"
-import React from "react";
+'use client'
+import React, {useEffect} from "react";
 import DocViewer, {DocViewerRenderers} from "react-doc-viewer";
 
-if (typeof Promise.withResolvers === 'undefined') {
-    if (window)
-        // @ts-expect-error This does not exist outside of polyfill which this is doing
-        window.Promise.withResolvers = function () {
-            let resolve, reject;
-            const promise = new Promise((res, rej) => {
-                resolve = res;
-                reject = rej;
-            });
-            return { promise, resolve, reject };
-        };
-}
-
 const Docs = () => {
-    const url = `${window.location.protocol}//${window.location.host}`
-    const docs = [
-        { uri: `${url}/ДОГОВІР_ПУБЛІЧНОЇ_ОФЕРТИ_ПРО_НАДАННЯ_ІНФОРМАЦІЙНИХ_ПОСЛУГ.docx` },
-        { uri: `${url}/Правила повернення грошових коштів.docx` }, // Local File
-    ];
+
+    const [ docs, setDocs ] = React.useState<{ uri: string }[]>([]);
+
+
+    useEffect(() => {
+        const url = `${window.location.protocol}//${window.location.host}`
+        const docs = [
+            { uri: `${url}/ДОГОВІР_ПУБЛІЧНОЇ_ОФЕРТИ_ПРО_НАДАННЯ_ІНФОРМАЦІЙНИХ_ПОСЛУГ.docx` },
+            { uri: `${url}/Правила повернення грошових коштів.docx` }, // Local File
+        ];
+        setDocs(docs);
+    }, []);
 
     return <DocViewer pluginRenderers={DocViewerRenderers} documents={docs} />
 }
