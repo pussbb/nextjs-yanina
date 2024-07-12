19181a1ed973c69257ce699f60e2a4a6fd5c464a
pussbb <pussbb@gmail.com>
2024-07-12 11:19:47 +0200

    email fix

--- app/offerta/page.tsx
+++ app/offerta/page.tsx
@@ -1,6 +1,8 @@
 import { Metadata } from 'next';
 import generateDefaultMetadata from '@/app/metadata';
 import Link from "next/link";
+import RefundRules from "@/app/Sections/RefundRules";
+import Rules from "@/app/offerta/rules";
 
 
 export const metadata: Metadata = generateDefaultMetadata('Правила повернення грошей')
@@ -11,20 +13,26 @@ const Page = () => {
         <main className="flex min-h-screen flex-col justify-between p-1">
             <section className="section pb-0">
                 <div className="container">
-                    <br/>
-                    <Link className="cta-link inline-flex items-center text-primary raspberry-color"
-                          href={"/ДОГОВІР_ПУБЛІЧНОЇ_ОФЕРТИ_ПРО_НАДАННЯ_ІНФОРМАЦІЙНИХ_ПОСЛУГ.docx"}>
-                        Загрузити документ &quot;Договір публічної оферти&quot;
-                    </Link>
-                    <br/>
-                    <Link className="cta-link inline-flex items-center text-primary raspberry-color"
-                          href={"/Правила повернення грошових коштів.docx"}>
-                        Загрузити документ &quot;Правила повернення грошових коштів&quot;
-                    </Link>
+                    <div className="items-center">
+                        <br/>
+                        <Rules />
+                        <Link className="cta-link inline-flex items-center text-primary raspberry-color"
+                              href={"/ДОГОВІР_ПУБЛІЧНОЇ_ОФЕРТИ_ПРО_НАДАННЯ_ІНФОРМАЦІЙНИХ_ПОСЛУГ.docx"}>
+                            Загрузити документ &quot;Договір публічної оферти&quot;
+                        </Link>
+                        <br/>
+                    </div>
+                    <div className="items-center">
+                        <RefundRules/>
+                        <Link className="cta-link inline-flex items-center text-primary raspberry-color"
+                              href={"/Правила повернення грошових коштів.docx"}>
+                            Загрузити документ
+                        </Link>
+                    </div>
                 </div>
             </section>
         </main>
-    )
+)
 }
 
 export default Page
